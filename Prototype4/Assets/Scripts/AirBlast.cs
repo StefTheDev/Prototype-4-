2765672df3b8d717b21cda64a158baeaa070862e
Tim <timbbby@gmail.com>
2019-08-28 14:30:30 +1200

    First gameplay iteration

---
+++ Prototype4/Assets/Scripts/AirBlast.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AirBlast : MonoBehaviour
+{
+    public float blastSpeed = 10.0f;
+    public float maxLifetime = 3.0f;
+    public float blastForce = 10.0f;
+
+    private Rigidbody rigidBody;
+    private Vector3 direction;
+    private int playerIndex;
+    private float chargeAmount;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void Launch(Vector3 _launchDirection, float _chargeAmount, int _playerIndex)
+    {
+        rigidBody.AddForce(_launchDirection * _chargeAmount * blastSpeed);
+        direction = _launchDirection;
+        playerIndex = _playerIndex;
+        chargeAmount = _chargeAmount;
+        GameObject.Destroy(this.gameObject, maxLifetime * _chargeAmount);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var otherPlayer = other.GetComponent<Player>();
+
+        if (otherPlayer && otherPlayer.GetPlayerID() != playerIndex)
+        {
+            otherPlayer.GetComponent<Rigidbody>().AddForce(direction * blastForce * chargeAmount);
+            Destroy(this.gameObject);
+        }
+    }
+}
