5b9400a2a9806e419ed82b333d4965d71c2eb65b
StefTheDev <steffthedev@gmail.com>
2019-08-28 14:21:55 +1200

    Timer, SpawnPoints, Deathbox Update
    + Timer Text
    + Deathbox bounds
    + Spawnpoints | Gizmos, positions.
    + GameManager
    + Post Processing built in package
    ~ Changed Post Processing values
    - Post Processing Package from asset store.

---
+++ Prototype4/Assets/Scripts/SpawnPoints.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+    public List<SpawnPoint> spawnPoints;
+
+    public SpawnPoint GetRandom()
+    {
+        return spawnPoints[Random.Range(0, spawnPoints.Count)];
+    }
+}
