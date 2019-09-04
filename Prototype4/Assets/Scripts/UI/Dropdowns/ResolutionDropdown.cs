c676e3ae6cfc3ccd811351be31e6c78738d8d6ee
StefTheDev <steffthedev@gmail.com>
2019-09-04 12:03:07 +1200

    UI Update

---
+++ Prototype4/Assets/Scripts/UI/Dropdowns/ResolutionDropdown.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResolutionDropdown : UIDropdown<Resolution>
+{
+    private void Start()
+    {
+        Initialise(new List<Resolution>(Screen.resolutions));
+    }
+
+
+    public override void SelectOption()
+    {
+        Resolution resolution = GetElement(dropdown.value);
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen, resolution.refreshRate);
+    }
+}
