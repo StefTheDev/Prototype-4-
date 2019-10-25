7c2ba7984c829563aff6882d4e50ac0f98cd2ad4
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-10-25 15:42:01 +1300

    Shield Bubble particle system
    + DOTween
    + particle system
    + on player prefab

---
+++ Prototype4/Assets/Scripts/Powerups/ShieldBubble.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShieldBubble : MonoBehaviour
+{
+	public Transform particles;
+
+	private void Start()
+	{
+		particles.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Q)) // DEBUG
+		{
+			particles.DOScale(new Vector3(1, 1, 1), 1).SetEase(Ease.OutElastic);
+		}
+	}
+
+	private void LateUpdate()
+	{
+		transform.LookAt(Camera.main.transform);
+	}
+}
