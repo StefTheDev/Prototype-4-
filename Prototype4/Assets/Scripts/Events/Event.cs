c5ff45cfddfa0fe650b4bcdbb4a794d0c2ed3a55
StefTheDev <steffthedev@gmail.com>
2019-10-30 10:35:18 +1300

    Events
    + Events Base Class
    + Events Management Class
    
    Each event has a built in unity that it can call. An event call will happen whenever the next item is schedulled and removed from the queue.

---
+++ Prototype4/Assets/Scripts/Events/Event.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public abstract class Event : MonoBehaviour
+{
+    [SerializeField] private string name;
+    private UnityEvent unityEvent;
+
+    private void Start()
+    {
+        if (unityEvent == null) unityEvent = new UnityEvent();
+        unityEvent.AddListener(OnEvent);
+    }
+
+    public void Call()
+    {
+        unityEvent.Invoke();
+    }
+
+    public abstract void OnEvent();
+}
---
+++ Prototype4/Assets/Scripts/Events/EventsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsManager : MonoBehaviour
+{
+    [SerializeField] private int delay;
+    private Queue<Event> events;
+
+    private void Start()
+    {
+        events = new Queue<Event>();
+    }
+
+    public void Run()
+    {
+        StartCoroutine(ExecuteAfterTime(delay));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        events.Dequeue().Call();
+        StartCoroutine(ExecuteAfterTime(time));
+    }
+
+    public Queue<Event> GetEvents()
+    {
+        return events;
+    }
+}
