ca316a144d0a103a028fbe468e9cc4a9a2940217
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-10-30 10:39:03 +1300

    hit fx yay

---
+++ Prototype4/Assets/Scripts/Powerups/HitEffectsSpawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitEffectsSpawner : MonoBehaviour
+{
+    public ShieldPowerup player;
+    public GameObject prefab;
+
+    private void Awake()
+    {
+        player.onHit += (args) => Instantiate(prefab, args.point, Quaternion.LookRotation(args.normal));
+    }
+}
