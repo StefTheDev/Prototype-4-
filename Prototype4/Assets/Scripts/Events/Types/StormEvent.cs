b4503e795158ed5b75a54d78322da5558eca8781
StefTheDev <Steffthedev@gmail.com>
2019-11-06 07:48:05 +1300

    Update

--- Prototype4/Assets/Scripts/Events/Types/StormEvent.cs
+++ Prototype4/Assets/Scripts/Events/Types/StormEvent.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class StormEvent : Event
 {
-    [SerializeField] private GameObject lightning;
+    [SerializeField] private GameObject lightning, rain;
 
     public bool includeChildren = true;
 
@@ -12,11 +12,13 @@ public class StormEvent : Event
     {
         Debug.Log("Storm Event Started.");
         lightning.SetActive(true);
+        rain.SetActive(true);
     }
 
     public override void OnEnd()
     {
         Debug.Log("Storm Event Ended.");
         lightning.SetActive(false);
+        rain.SetActive(false);
     }
 }
