522c189aaa9864444188d684a7c8e19b7e9f1feb
Elijah Shadbolt <elijah.shadbolt@gmail.com>
2019-11-13 09:46:47 +1300

    barrels spawn every 30 seconds

---
+++ Prototype4/Assets/Scripts/BarrelThrowerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelThrowerManager : MonoBehaviour
+{
+    public float seconds = 30;
+    public BarrelThrower thrower;
+
+    private void Awake()
+    {
+        if (!thrower) { thrower = GetComponent<BarrelThrower>(); }
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.onGameStarted += () => StartCoroutine(SpawnBarrelsLoop());
+    }
+
+    IEnumerator SpawnBarrelsLoop()
+    {
+        while (true)
+        {
+            thrower.SpawnBarrels();
+            yield return new WaitForSeconds(seconds);
+        }
+    }
+}
