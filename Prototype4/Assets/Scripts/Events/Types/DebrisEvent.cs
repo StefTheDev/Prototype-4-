3a128b2140e32c089e79bfc49799711a222feef9
Tim <timbbby@gmail.com>
2019-11-13 09:35:29 +1300

    Added log spawning to debris event

--- Prototype4/Assets/Scripts/Events/Types/DebrisEvent.cs
+++ Prototype4/Assets/Scripts/Events/Types/DebrisEvent.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DebrisEvent : Event
 {
-
+    int numLogs = 3;
     public override void OnEnd()
     {
 
@@ -12,6 +12,15 @@ public class DebrisEvent : Event
 
     public override void OnStart()
     {
+        for (int i = 0; i < numLogs; i++)
+        {
+            float spawnDistance = Random.Range(0.0f, 9.0f);
+            float spawnAngle = Random.Range(0.0f, 360.0f) * Mathf.Deg2Rad;
+            float spawnHeight = Random.Range(50.0f, 100.0f);
 
+            Vector3 spawnPos = new Vector3(spawnDistance * Mathf.Cos(spawnAngle), spawnHeight, spawnDistance * Mathf.Sin(spawnAngle));
+
+            Instantiate(ReferenceManager.Instance.logPrefab, spawnPos, Quaternion.identity, null);
+        }
     }
 }
